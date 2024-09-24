2d105ae1acba06b2f9d9761a06f93c0ffe01d6d1
rubian64 <121987277+rubian64@users.noreply.github.com>
2024-09-24 14:41:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 import streamlit as st
+import hashlib
 import sqlite3
 import pandas as pd
 
+# Dados de usuário (substitua por um banco de dados real)
+users = {
+    "usuario1": hashlib.sha256("senha123".encode()).hexdigest(),
+    "usuario2": hashlib.sha256("senha456".encode()).hexdigest(),
+}
 
-def main():
-    st.title("CANA EXPRESS")
+def login():
+    st.title("Tela de Login")
 
-if __name__ == '__main__':
-    main()
+    username = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
+    if st.button("Entrar"):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        if username in users and users[username] == hashed_password:
+            st.success("Login realizado com sucesso!")
+            # Redirecionar para a área de cliente
+            st.write("Bem-vindo(a),", username)
+        else:
+            st.error("Usuário ou senha inválidos.")
+
+
+if __name__ == "__main__":
+    login()
